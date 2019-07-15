991a9647f923d011348db6a998ce628378164263
chihchien49 <52899041+chihchien49@users.noreply.github.com>
2019-07-15 11:39:20 +0800

    Update sample.py

--- sample.py
+++ sample.py
@@ -46,7 +46,7 @@ def handle_message(event):
             original_content_url='https://i.imgur.com/LWyvy2Y.jpg',
             preview_image_url='https://i.imgur.com/LWyvy2Y.jpg'
         )
-        line_bot_api.reply_message(event.reply_token,message)
+      
 
 
 if __name__ == "__main__":
