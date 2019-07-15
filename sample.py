e10f2b606ab0312feff5361158c8610f795b19f2
chihchien49 <52899041+chihchien49@users.noreply.github.com>
2019-07-15 11:24:27 +0800

    Update sample.py

--- sample.py
+++ sample.py
@@ -43,8 +43,8 @@ def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
         message=ImageSendMessage(
-            original_content_url='https://imgur.com/a/IRX42pJ',
-            preview_image_url='https://imgur.com/a/IRX42pJ'
+            original_content_url='https://imgur.com/LWyvy2Y',
+            preview_image_url='https://imgur.com/LWyvy2Y'
         )
         line_bot_api.reply_message(event.reply_token,message)
 
